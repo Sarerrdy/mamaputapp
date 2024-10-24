ea095adc4b0ecee4e5e815c346ba19ef3ce399f1
sarerrdy <sarerrdy4live@gmail.com>
2024-10-24 05:17:47 +0100

    unified notification

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -89,6 +89,7 @@ const LoginForm = () => {
             <input
               type="text"
               id="username"
+              placeholder="Enter email"
               {...register("username", { required: "Username is required" })}
               className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-200 focus:border-indigo-300"
             />
