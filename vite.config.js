8a5e584104fb82724fdaceaed13537b8766ea305
sarerrdy <sarerrdy4live@gmail.com>
2024-12-17 14:43:15 +0100

    server.js conf

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/upload': 'http://localhost:3000' } }
-})
+  server: {
+    proxy: { "/upload": "http://localhost:3000" },
+    host: "0.0.0.0", // Ensure Vite binds to 0.0.0.0
+    port: process.env.PORT || 5173, // Use the environment variable PORT if available
+  },
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: { proxy: { '/upload': 'http://localhost:3000' } }
+// })
